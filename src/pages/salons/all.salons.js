e7700f48d167214620f26180fe9da6c8326a5f6d
Wanderley Neto <wneto_ramos@hotmail.com>
2021-05-22 21:13:51 -0300

    Criação de novas paginas na estrutura de pastas

---
+++ src/pages/salons/all.salons.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text,SafeAreaView} from 'react-native'
+
+export default function SalonsPage(){
+	return (
+		<SafeAreaView>
+			<Text>Salons page</Text>
+		</SafeAreaView>
+	)
+}
---
+++ src/pages/serves/all.serves.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text,SafeAreaView} from 'react-native'
+
+export default function Serves(){
+	return (
+		<SafeAreaView>
+			<Text>Serves page</Text>
+		</SafeAreaView>
+	)
+}
