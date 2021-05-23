61874764aeeb41857450af1541368bc755c10b87
Wanderley Neto <wneto_ramos@hotmail.com>
2021-05-22 23:11:25 -0300

    Tratamento para criação de procedimentos/ Função de ação genérica

--- src/pages/procedures/detail.procedures.page.js
+++ src/pages/procedures/detail.procedures.page.js
@@ -1,58 +1,69 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import {ScrollView, StatusBar, SafeAreaView, StyleSheet, Text, View, TextInput } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+// import {  } from 'react-native-gesture-handler'
 
 export default function DetailProcedure({ route, navigation }) {
 
 
-	const { procedure, operation, insertAction, editAction, delAction } = route.params;
+	const { procedure, operation, buttonAction } = route.params;
 
-	const [description, setDescription] = useState(!!procedure.description ? procedure.description : "")
-	const [value, setValue] = useState(!!procedure.value ? procedure.value : 0)
+	const [description, setDescription] = useState(!!procedure?.description ? procedure.description : "")
+	const [value, setValue] = useState(!!procedure?.value ? procedure.value : 0)
 
-	async function handleInsert() {
+	async function handleAction() {
 
-		const newProcedure = {
-			description,
-			value
+		if (operation === 'delete'){
+			await buttonAction(procedure?.id)
+		} else {
+
+			const editProcedure = {
+				...procedure,
+				description,
+				value
+			}
+	
+			// executa método de edição do procediento
+			await buttonAction(editProcedure)
+
 		}
 
-		// executa método de gravação do procedimento
-		await insertAction(newProcedure)
 		// rediciona para 'All Procedures'
 		navigation.navigate('procedures', { screen: 'all.procedures' });
 
 	}
-
-	async function handleEdit() {
-
-		const editProcedure = {
-			...procedure,
-			description,
-			value
-		}
-
-		// executa método de edição do procediento
-		await editAction(editProcedure)
-		// rediciona para 'All Procedures'
-		navigation.navigate('procedures', { screen: 'all.procedures' });
-
-	}
-
-	async function handleDelete() {
-
-		// executa método de exclusão do procedimento
-		await delAction(procedure.id);
-		// rediciona para 'All Procedures'
-		navigation.navigate('procedures', { screen: 'all.procedures' });
-
-	}
-
 
 	return (
-		<View>
-			<Text>Detail Procedure</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<ScrollView style={styles.scrollView} >
+				<TextInput style={styles.text} onChangeText={(t)=>setDescription(t)} placeholder="Descrição" />
+				<TextInput style={styles.text} onChangeText={(t)=>setValue(t)} placeholder="Valor" />
+				<TouchableOpacity style={styles.button} onPress={handleAction}>
+					<Text>{operation === 'insert' ? 'Criar' : 'Salvar Alterações'}</Text>
+				</TouchableOpacity>
+			</ScrollView>
+
+		</SafeAreaView>
 	)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingTop: StatusBar.currentHeight,
+	},
+	scrollView: {
+		
+	},
+	text: {
+		fontSize: 12,
+	},
+	button:{
+		width: '90%',
+		height: 40,
+		backgroundColor: 'gray',
+
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+});
