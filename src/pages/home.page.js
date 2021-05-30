0f280c765cf0a3d424be3bb8e1467bce71c4e128
Wanderley Neto <wneto_ramos@hotmail.com>
2021-05-30 16:27:22 -0300

    Implementação dos botões "Novo cliente" e "Novo Atendimento"

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -2,52 +2,87 @@ import React from 'react'
 import { ScrollView, SafeAreaView, StatusBar, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import logo from '../../assets/005-beleza.png'
+import avatar from '../../assets/LogoPV_Preto.png'
 import { colors, metrics } from '../styles'
 import { useAuth } from '../contexts/user.context'
-
-function Card({ children, icon, size, color, text }) {
+import Header from '../components/header'
+import { useCustomers } from '../contexts/customers.context';
+import { useCustomerServices } from '../contexts/customerService.context';
+function Card({ children, icon, size, color, text, onPress }) {
 	return (
 		<View style={styles.cardContainer}>
-			<TouchableOpacity style={styles.button}>
+			<TouchableOpacity style={styles.button} onPress={onPress}>
 				<Icon name={icon} size={size} color={color}></Icon>
 				<Text style={styles.textButton}>{text}</Text>
 			</TouchableOpacity>
 		</View>
 	)
 }
-export default function HomePage() {
+export default function HomePage({ navigation }) {
 	const { user } = useAuth()
+	const { createCustomer } = useCustomers();
+	const { createCustomerService } = useCustomerServices();
+
+	function goToNewCustomer() {
+
+		navigation.navigate('customers', {
+			screen: 'new.customer',
+			params: {
+				operation: 'insert',
+				buttonAction: createCustomer
+			}
+		})
+	}
+
+	function goToNewCustomerService() {
+
+		navigation.navigate('config', {
+			screen: 'customerServices',
+			params: {
+				screen: 'new.customerService',
+				params: {
+					operation: 'insert',
+					buttonAction: createCustomerService
+				}
+			}
+		})
+	}
 
 	return (
 
 		<SafeAreaView style={styles.background}>
 			<StatusBar
-				barStyle="dark-content"
+				barStyle={colors.statusBarStyle}
 				backgroundColor={colors.background}
 			/>
+			<Header />
 			<View style={styles.container}>
-
-
 				<View style={styles.containerHeader}>
-					<Text style={styles.textHello}>Olá</Text>
-					<Text style={styles.textName}>{user.name}!</Text>
+					<View>
+						<Text style={styles.textHello}>Olá</Text>
+						<Text style={styles.textName}>{user.name}!</Text>
+					</View>
+					<View style={styles.avatarContainer}>						
+						<Image source={avatar} style={styles.avatar} />
+					</View>
 				</View>
 
-				<View style={{ width: '100%', height: '25%', alignItems: 'center' }}>
+				<View style={{ width: '100%', height: '60%', alignItems: 'center' }}>
 					<Image source={logo} style={styles.img} />
 				</View>
+			</View>
+
+			<View style={styles.container}>
 				<Text>O que deseja fazer?</Text>
-
-
 				<View style={styles.containerButtons}>
 
-					<ScrollView horizontal={true} style={{ height: '30%', width: '100%' }} >
+					{/* <ScrollView horizontal={true} style={{ height: '30%', width: '100%' }} > */}
 
-						<Card icon="brush" size={48} color="#999" text="Novo Atendimento" />
-						<Card icon="person" size={48} color="#999" text="Nova Cliente" />
-						<Card icon="money" size={48} color="#999" text="Finanças" />
-						
-					</ScrollView>
+					<Card icon="brush" size={48} color={colors.actionButton} text="Novo Atendimento" onPress={goToNewCustomerService} />
+					<Card icon="person" size={48} color={colors.actionButton} text="Nova Cliente" onPress={goToNewCustomer} />
+					{/* <Card icon="money" size={48} color="#999" text="Finanças" /> */}
+
+					{/* </ScrollView> */}
 				</View>
 
 			</View>
@@ -59,39 +94,52 @@ export default function HomePage() {
 const styles = StyleSheet.create({
 	background: {
 		height: '100%',
-		backgroundColor: colors.background
+		backgroundColor: colors.boxBackground
 	},
 	container: {
-		margin: 7,
+		marginLeft: 20,
+		marginRight: 20,
+		marginBottom: 20,
 		padding: 10,
-		height: '96%', // Corrigir condiderando 100% menos altura da TabBar
-		borderRadius: metrics.borderRadius,
+		height: '42%', // Corrigir condiderando 100% menos altura da TabBar
+		borderRadius: 10,
 		justifyContent: 'space-around',
-		// backgroundColor: colors.boxBackground,
+		backgroundColor: colors.background,
+	},
+	containerHeader: {
+		flexDirection: 'row',
+		width: '100%',
+		height: '30%',
+		justifyContent: 'space-between',
 	},
 	img: {
 		height: '100%',
 		resizeMode: 'contain'
 	},
+	avatarContainer: {
+		// flex: 1,
+		width: 75,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: colors.boxBackground,
+		borderRadius: 50,
+	},
+	avatar: {
+		// flex: 1,
+		height: '100%',
+		resizeMode: 'contain',
+	},
 	cardContainer: {
-		width: '50%',
-		height: '85%',
-		backgroundColor: '#FFF',
+		width: '45%',
+		height: '100%',
+		backgroundColor: colors.boxBackground,
 		justifyContent: 'center',
 		alignItems: 'center',
 		margin: 10,
 		// marginBottom: 20,
 
 		borderRadius: metrics.borderRadius,
-		borderColor: '#d2d2d2',
 		elevation: 10,
-		// borderWidth: 10,
-		// shadowOffset: {
-		// 	width: 10,
-		// 	height: 10
-		// }
-
-
 	},
 	textHello: {
 		fontSize: 22,
@@ -101,8 +149,8 @@ const styles = StyleSheet.create({
 		fontWeight: 'bold'
 	},
 	containerButtons: {
-
-		height: '35%',
+		flexDirection: 'row',
+		height: '70%',
 		width: '100%'
 		// alignItems: 'center',
 		// justifyContent: 'space-around',
@@ -121,6 +169,6 @@ const styles = StyleSheet.create({
 	},
 	textButton: {
 		fontSize: 12,
-		color: '#555'
+		color: colors.actionButton
 	}
 })
