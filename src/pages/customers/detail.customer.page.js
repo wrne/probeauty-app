145f7b1fd545b2ba67c2d8b4b3bc5de6b5832802
Wanderley Neto <wneto_ramos@hotmail.com>
2021-05-30 21:52:44 -0300

    Adaptando cadastro de clientes para o Styled Components/ Switch/ Estilo básico

--- src/pages/customers/detail.customer.page.js
+++ src/pages/customers/detail.customer.page.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import {ScrollView, StatusBar, SafeAreaView, StyleSheet, Text, View, TextInput } from 'react-native'
+import { ScrollView, StatusBar, SafeAreaView, StyleSheet, Text, View, Switch } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 // import {  } from 'react-native-gesture-handler'
+import { Input, ActionButton } from '../../components/basics';
 
 export default function DetailCustomer({ route, navigation }) {
 
@@ -13,15 +14,14 @@ export default function DetailCustomer({ route, navigation }) {
 	const [phone, setPhone] = useState(!!customer?.phone ? customer.phone : '')
 	const [email, setEmail] = useState(!!customer?.email ? customer.email : '')
 	const [discount, setDiscount] = useState(!!customer?.discount ? customer.discount : '')
-	const [canUsePhoto, setCanUsePhoto] = useState(!!customer?.canUsePhoto ? customer.canUsePhoto : '')
+	const [canUsePhoto, setCanUsePhoto] = useState(!!customer?.canUsePhoto ? customer.canUsePhoto : true)
 	const [anamnese, setAnamnese] = useState(!!customer?.anamnese ? customer.anamnese : '')
 	const [mappingType, setMappingType] = useState(!!customer?.mappingType ? customer.mappingType : '')
 	const [mapping, setMapping] = useState(!!customer?.mapping ? customer.mapping : '')
 	const [thickness, setThickness] = useState(!!customer?.thickness ? customer.thickness : '')
 	const [curvature, setCurvature] = useState(!!customer?.curvature ? customer.curvature : '')
-	
-	async function handleAction() {
 
+	async function handleAction() {
 
 		const editCustomer = {
 			...customer,
@@ -37,7 +37,7 @@ export default function DetailCustomer({ route, navigation }) {
 			thickness,
 			curvature
 		}
-		
+
 		// executa método de edição do procediento
 		await buttonAction(editCustomer)
 
@@ -51,24 +51,37 @@ export default function DetailCustomer({ route, navigation }) {
 		<SafeAreaView style={styles.container}>
 			<ScrollView style={styles.scrollView} >
 
-				<TextInput style={styles.text} onChangeText={(t)=>setName(t)} placeholder="Name" value={name}/>
-				<TextInput style={styles.text} onChangeText={(t)=>setBirth(t)} placeholder="Birth" value={birth}/>
-				<TextInput style={styles.text} onChangeText={(t)=>setPhone(t)} placeholder="Phone" value={phone}/>
-				<TextInput style={styles.text} onChangeText={(t)=>setEmail(t)} placeholder="Email" value={email}/>
-				<TextInput style={styles.text} onChangeText={(t)=>setDiscount(t)} placeholder="Discount" value={discount}/>
-				<TextInput style={styles.text} onChangeText={(t)=>setCanUsePhoto(t)} placeholder="CanUsePhoto" value={canUsePhoto}/>
-				<TextInput style={styles.text} onChangeText={(t)=>setAnamnese(t)} placeholder="Anamnese" value={anamnese}/>
-				<TextInput style={styles.text} onChangeText={(t)=>setMappingType(t)} placeholder="MappingType" value={mappingType}/>
-				<TextInput style={styles.text} onChangeText={(t)=>setMapping(t)} placeholder="Mapping" value={mapping}/>
-				<TextInput style={styles.text} onChangeText={(t)=>setThickness(t)} placeholder="Thickness" value={thickness}/>
-				<TextInput style={styles.text} onChangeText={(t)=>setCurvature(t)} placeholder="Curvature" value={curvature}/>
+				{/* <TextInput style={styles.text} onChangeText={(t)=>setName(t)} placeholder="Name" value={name}/> */}
+				<Text>Dados Pessoais</Text>
+				<Input onChangeText={(t) => setName(t)} placeholder="Name" value={name} />
+				<View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
 
-				<TouchableOpacity style={styles.button} onPress={handleAction}>
-					<Text>{operation === 'insert' ? 'Criar' : 'Salvar AltArações'}</Text>
-				</TouchableOpacity>
-		</ScrollView>
+					<Input style={{ width: '40%' }} onChangeText={(t) => setBirth(t)} placeholder="Birth" value={birth} />
+					<Input style={{ width: '55%' }} onChangeText={(t) => setPhone(t)} placeholder="Phone" value={phone} />
+				</View>
+				<Input onChangeText={(t) => setEmail(t)} placeholder="Email" value={email} />
+				<Text>Dados Comerciais</Text>
+				<Input style={{ width: '50%' }} onChangeText={(t) => setDiscount(t)} placeholder="Discount" value={discount} />
+				{/* <Input onChangeText={(t)=>setCanUsePhoto(t)} placeholder="CanUsePhoto" value={canUsePhoto}/> */}
+				<View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+					<Text>Pode usar a foto?</Text>
+					<Switch onValueChange={(newValue) => setCanUsePhoto(newValue)} value={canUsePhoto} />
+				</View>
+				<Text>Cílios</Text>
+				<Input onChangeText={(t) => setMappingType(t)} placeholder="MappingType" value={mappingType} />
+				<Input onChangeText={(t) => setMapping(t)} placeholder="Mapping" value={mapping} />
+				<Input onChangeText={(t) => setThickness(t)} placeholder="Thickness" value={thickness} />
+				<Input onChangeText={(t) => setCurvature(t)} placeholder="Curvature" value={curvature} />
 
-	</SafeAreaView>	)
+				<View style={{ alignItems: 'center' }}>
+
+					<ActionButton style={styles.button} onPress={handleAction}>
+						<Text>{operation === 'insert' ? 'Criar' : 'Salvar AltArações'}</Text>
+					</ActionButton>
+				</View>
+			</ScrollView>
+
+		</SafeAreaView>)
 }
 
 const styles = StyleSheet.create({
@@ -77,17 +90,12 @@ const styles = StyleSheet.create({
 		paddingTop: StatusBar.currentHeight,
 	},
 	scrollView: {
-		
+		margin: 10,
 	},
 	text: {
 		fontSize: 12,
 	},
-	button:{
-		width: '90%',
+	button: {
 		height: 40,
-		backgroundColor: 'gray',
-
-		alignItems: 'center',
-		justifyContent: 'center'
 	},
 });
